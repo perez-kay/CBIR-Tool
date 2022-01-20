b827aac3319d0732f3c1b1bd1c1217c50c2e27a3
Kayla Perez <perezkay@uw.edu>
2022-01-19 21:32:38 -0800

    commented

--- UI.py
+++ UI.py
@@ -1,47 +1,82 @@
 import streamlit as st
 from PIL import Image
-import numpy as np
-import os, cbir_methods, paginator
+import cbir_methods, paginator
 
 st.set_page_config(layout='wide', page_title='CBIR Tool')
 
 def get_image_path(img_num):
     """
-    Returns the image path for the given image number.
+    Creates a file path for the given image number.
+
+    Parameters
+    ----------
+    img_num : float
+        The image number
+    
+    Returns
+    -------
+    str
+        A valid file path for the image in the form of "images/<img_num>.jpg"
     """
+
     return "images/" + str(int(img_num)) + ".jpg"
 
 
-st.title("Content-Based Image Retrieval")
+# Create page title and introduction
+st.title("Content-Based Image Retrieval Tool")
+st.write("Welcome to the CBIR Tool! To view the images in the database, type a number into the box below. \
+         Once you've selected an image, use the drop down menu to choose a CBIR method. \
+         Then click \'Retrieve Images\' to retrieve images based on the method you chose. \
+         Your retrieval results will be displayed to the right.")
 
+# Create 2 columns 
 left_col, right_col = st.columns(2)
 
+# Display these items in the left column
 with left_col:
-    # select an image and display it. Displays image 1 by default
+    # Select an image and display it. Displays image 1 by default
     img_num = st.number_input("Select an image by typing a number between 1 - 100", min_value=1, max_value=100, step=1)
     img_path = get_image_path(img_num)
     st.image(image=(img_path), use_column_width='always')
 
-
+    # Set up the select box for the color codes
     methods = ['-', "Intensity", "Color-Code"]
-
-    
     option = st.selectbox('Choose a method', methods)
 
-with right_col:
-    if option != '-':
-        if option == "Intensity":
-            with st.spinner('Fetching your results...'):
-                results = cbir_methods.get_distance(img_path, cbir_methods.calculate_intensity)
-            
-        if option == "Color-Code":
-            with st.spinner('Fetching your results...'):
-                results = cbir_methods.get_distance(img_path, cbir_methods.calculate_color_code)
+    # Set up the run button
+    run_checked = st.button("Retrieve Images")
 
-        img_results = [path for distance, path in results]
-        img_iter = paginator.paginator('View Results', img_results, items_per_page=20, on_sidebar=False)
+# Display these items in the right columm
+with right_col:
+    # If the button has been pressed
+    if run_checked:
+        if option == '-':
+            with left_col:
+                st.error("Please select a method first.")
+        else:
+            # Fetch results based on mode chosen
+            if option == "Intensity":
+                with st.spinner('Fetching your results...'):
+                    results = cbir_methods.get_distance(img_path, cbir_methods.calculate_intensity)
+            
+            if option == "Color-Code":
+                with st.spinner('Fetching your results...'):
+                    results = cbir_methods.get_distance(img_path, cbir_methods.calculate_color_code)
+            
+            # Update session state so it remembers results
+            st.session_state.results = results
+
+    # If the results exist
+    if st.session_state.results:
+        #Get the image paths for the results     
+        img_results = [path for distance, path in st.session_state.results]
+
+        # Create paginator to display results pages
+        img_iter = paginator.paginator('Retrieval Results', img_results, items_per_page=20, on_sidebar=False)
         indicies, imgs = map(list, zip(*img_iter))
-        st.image(imgs, width=150, caption=imgs)
+
+        # Display image results
+        st.image(imgs, width=150, caption=imgs)  
 
 
 
--- cbir_methods.py
+++ cbir_methods.py
@@ -1,42 +1,76 @@
-from select import select
 from PIL import Image as img
-import matplotlib.pyplot as plt
 import numpy as np
 import os
 import streamlit as st
 
 def get_intensity(vals):
+    """
+    Calculates the intesity for the given RGB values.
+
+    Parameters
+    ----------
+    vals : tuple
+        The RGB values, in RGB order
+
+    Returns
+    -------
+    float
+        The intensity value
+    """
+
     return (.299*vals[0]) + (.587*vals[1]) + (.114 * vals[2])
 
 
 def calculate_intensity(img_path):
     """
-    Calculates the intensity for each pixel of the given image.
-    Returns, as a list, the histogram bins for each intensity.
+    Calculates the intensity for each pixel of the given image and creates
+    a histogram.
+    
+    Parameters
+    ----------
+    img_path : str
+        The file path for the image
+    
+    Returns
+    -------
+    tuple
+        The first value contains the image size, second value contains the
+        intensity histogram
     """
+
     pic = img.open(img_path)
     width, height = pic.size
-    #pixels = list(pic.getdata())
-    #pixels = np.array(pixels).reshape((width, height, 3))
-
     intensities = list()
     bins = list(range(0,250,10))
     bins.append(255)
+
     for i in range(width):
         for j in range(height):
             intensities.append(get_intensity(pic.getpixel((i,j))))
-            
+
     hist, bin_edges = np.histogram(intensities, bins=bins)
     return (width*height, hist)
 
-
-
     
 def calculate_color_code(img_path):
+    """
+    Calculates the color-code for each pixel of the given image and creates
+    a histogram.
+    
+    Parameters
+    ----------
+    img_path : str
+        The file path for the image
+    
+    Returns
+    -------
+    tuple
+        The first value contains the image size, second value contains the
+        intensity histogram
+    """
+
     pic = img.open(img_path)
     width, height = pic.size
-    #pixels = list(pic.getdata())
-    #pixels = np.array(pixels).reshape((width, height, 3)) 
     binaries = list()
 
     for i in range(width):
@@ -49,11 +83,29 @@ def calculate_color_code(img_path):
             binary = r[0:2] + g[0:2] + b[0:2]
             decimal = int(binary, 2)
             binaries.append(decimal)
+
     hist, bin_edges = np.histogram(binaries, bins=64)
-    
     return (width*height, hist)
 
 def calculate_manhattan(img1, img2):
+    """
+    Calculates the Manhattan Distance between the two given lists. The lists
+    must be of the same length
+
+    Parameters
+    ----------
+    img1 : list
+        The histogram for the first image
+    img2 : list
+        The histogram for the second image
+
+    Returns
+    -------
+    float:
+        The calculated Manhattan Distance
+    
+    """
+
     if len(img1) != len(img2):
         print("Error, lengths don't match up")
         return -1
@@ -64,29 +116,30 @@ def calculate_manhattan(img1, img2):
         h_k = img2[1][j]
         size_i = img1[0]
         size_k = img2[0]
-        #if size_i == size_k:
-         #   result += abs((h_i  - h_k))
-        #else:
-
         result += abs((h_i / size_i) - (h_k / size_k))
     return result
 
-#@st.cache
-# def get_intensity_distance(selected_img):
-#     selected_intensity = calculate_intensity(selected_img)
-#     result = list()
-#     for img in os.listdir("images/"):
-#         if img == 'Thumbs.db':
-#             continue
-#         path = 'images/' + img
-#         if path != selected_img:
-#             intensity = calculate_intensity(path)
-#             distance = calculate_manhattan(selected_intensity, intensity)
-#             result.append((distance, path))
-#     return sorted(result)
-
-@st.cache(show_spinner=False)
+@st.cache(show_spinner=False) # used by streamlit to cache results
 def get_distance(selected_img, method):
+    """
+    Calcuates the Manhattan Distance between the selected image and all other
+    images in the "images/" directory based on the chosen CBIR method.
+
+    Parameters
+    ----------
+    selected_img : str
+        The file path of the selected image
+    method : function
+        The chosen CBIR method (either calculate_intensity or calculate_color_code)
+    
+    Returns
+    -------
+    list
+        A sorted list of tuples containing all of the distances. The first
+        value in each tuple is the distance. The second is the image path.
+        The list is sorted in ascending order based on distance.
+    """
+    
     selected = method(selected_img)
     result = list()
     for img in os.listdir("images/"):
@@ -98,15 +151,6 @@ def get_distance(selected_img, method):
             distance = calculate_manhattan(selected, retrieved)
             result.append((distance, path))
     return sorted(result)
-
-img1 = calculate_color_code('images/1.jpg')
-img2 = calculate_color_code('images/41.jpg')
-img3 = calculate_color_code('images/24.jpg')
-print(img1)
-print(img3)
-#print('The wrong img:', calculate_manhattan(img1, img2))
-
-#print('The right img:', calculate_manhattan(img1, img3))
 
 
 
