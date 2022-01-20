3320023f0fb842c6d4bbf4bc7ab249b857632f02
Kayla Perez <perezkay@uw.edu>
2022-01-19 21:33:00 -0800

    page now starts at 1

--- paginator.py
+++ paginator.py
@@ -33,8 +33,8 @@ def paginator(label, items, items_per_page=10, on_sidebar=True):
     items = list(items)
     n_pages = len(items)
     n_pages = (len(items) - 1) // items_per_page + 1
-    page_format_func = lambda i: "Page %s" % i
-    page_number = location.selectbox(label, range(n_pages), format_func=page_format_func)
+    page_format_func = lambda i: "Page %s" % (i + 1)
+    page_number = location.selectbox(label, range(n_pages), index=0, format_func=page_format_func)
 
     # Iterate over the items in the page to let the user display them.
     min_index = page_number * items_per_page
